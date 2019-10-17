66fdc1de61711b583b08207af9321ede3ad38b2a
harshalpatel165 <55267253+harshalpatel165@users.noreply.github.com>
2019-10-17 10:25:49 -0500

    created main.go and add packages in internal (#6)

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"github.com/SIGBlockchain/aurum_api/internal/endpoints"
+	"github.com/SIGBlockchain/aurum_api/internal/handlers"
+	"github.com/SIGBlockchain/aurum_api/internal/requests"
+	"github.com/SIGBlockchain/aurum_api/internal/resphandlers"
+)
